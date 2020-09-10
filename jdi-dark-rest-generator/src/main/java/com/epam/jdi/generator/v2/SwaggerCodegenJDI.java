a32022411cfbd5ab8e70c5fb4d71e5aa9f404532
Oksana Cherniavskaia <Oksana_Cherniavskaia@epam.com>
2020-09-10 19:05:43 +0300

    Switched to creating CLI with annotation style.

--- jdi-dark-rest-generator/src/main/java/com/epam/jdi/generator/v2/SwaggerCodegenJDI.java
+++ jdi-dark-rest-generator/src/main/java/com/epam/jdi/generator/v2/SwaggerCodegenJDI.java
@@ -2,25 +2,22 @@ package com.epam.jdi.generator.v2;
 
 import com.epam.jdi.generator.v2.cli.Generate;
 import com.github.rvesse.airline.Cli;
-import com.github.rvesse.airline.builder.CliBuilder;
 import com.github.rvesse.airline.help.Help;
 
+//If Jar name will be updated the name here should be changed as well
+
+@com.github.rvesse.airline.annotations.Cli(name = "java -jar DarkGenerator.jar", description = "Provides CLI to generate JDI Dark annotated classes from Open API/Swagger specification",
+        defaultCommand = Help.class, commands = {
+        Generate.class,
+        Help.class
+})
 public class SwaggerCodegenJDI {
 
 public static void main(String[] args) {
+    Cli<Runnable> cli = new Cli<>(SwaggerCodegenJDI.class);
     
-    //If Jar name will be updated the name here should be changed as well
-    
-    CliBuilder<Runnable> builder =
-            Cli.<Runnable>builder("java -jar DarkGenerator.jar").withDescription("JDI Dark Codegen Generator").withCommands(Generate.class, Help.class).withDefaultCommand(Help.class);
-    
-    // We can enable command and option abbreviation, this allows users to only
-    // type part of the group/command/option name provided that the portion they
-    // type is unambiguous
-    builder.withParser().withCommandAbbreviation().withOptionAbbreviation();
-    final Runnable generatorCli = builder.build().parse(args);
-    
-    generatorCli.run();
+    final Runnable parse = cli.parse(args);
+    parse.run();
     
 }
 }
