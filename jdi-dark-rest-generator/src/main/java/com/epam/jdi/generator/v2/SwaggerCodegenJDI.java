7bfdf8e9210f4cd6f248aa1ac7660d820bf96a43
Oksana Cherniavskaia <Oksana_Cherniavskaia@epam.com>
2020-09-10 18:43:53 +0300

    Airline CLI library version upgraded. Help system added to CLI execution. Maven plugin added to generate help on demand

--- jdi-dark-rest-generator/src/main/java/com/epam/jdi/generator/v2/SwaggerCodegenJDI.java
+++ jdi-dark-rest-generator/src/main/java/com/epam/jdi/generator/v2/SwaggerCodegenJDI.java
@@ -1,14 +1,26 @@
 package com.epam.jdi.generator.v2;
 
 import com.epam.jdi.generator.v2.cli.Generate;
-import io.airlift.airline.Cli;
+import com.github.rvesse.airline.Cli;
+import com.github.rvesse.airline.builder.CliBuilder;
+import com.github.rvesse.airline.help.Help;
 
 public class SwaggerCodegenJDI {
 
 public static void main(String[] args) {
-    @SuppressWarnings("unchecked") Cli.CliBuilder<Runnable> builder = Cli.<Runnable>builder("dark-generator").withDescription("JDI Dark Generator").withCommands(Generate.class);
     
-    final Runnable parse = builder.build().parse(args);
-    parse.run();
+    //If Jar name will be updated the name here should be changed as well
+    
+    CliBuilder<Runnable> builder =
+            Cli.<Runnable>builder("java -jar DarkGenerator.jar").withDescription("JDI Dark Codegen Generator").withCommands(Generate.class, Help.class).withDefaultCommand(Help.class);
+    
+    // We can enable command and option abbreviation, this allows users to only
+    // type part of the group/command/option name provided that the portion they
+    // type is unambiguous
+    builder.withParser().withCommandAbbreviation().withOptionAbbreviation();
+    final Runnable generatorCli = builder.build().parse(args);
+    
+    generatorCli.run();
+    
 }
 }
